15e4e017d4a0b29ab12a7e933c9c8bbdb24b3a12
Anlh-morgan <9.10hoangan.13072003@gmail.com>
2024-06-04 22:13:10 +0700

    update README

--- Chuong_1/BT5/bt5_C1.cpp
+++ Chuong_1/BT5/bt5_C1.cpp
@@ -1,15 +1,3 @@
-/*
-    Xây dựng cấu trúc dữ liệu để lưu trữ trong bộ nhớ trong(RAM) của máy
-    tính đa thức có bậc tự nhiên n (0<=n<=100) trên trường số thực(R)
-                fn(x) = a0 + a1*x + a2*x^2 + ... + an*x^n
-
-    Với cấu trúc dữ liệu đã xây dựng, trình bày thuật toán và thực hiện các việc sau:
-        - 1 nhập, xuất các đa thức
-        - 2 tính giá trị của đa thức tại một điểm x nào đó
-        - 3 tính tổng, tích của hai đa thức
-*/
-
-
 #include <iostream>
 #include <cmath>
 
