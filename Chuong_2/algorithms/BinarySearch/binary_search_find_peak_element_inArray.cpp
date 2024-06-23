1ce147613c15b58da84591c465bb7d319e51996e
Anlh-morgan <9.10hoangan.13072003@gmail.com>
2024-06-23 23:05:37 +0700

    binary search find Peak element

---
+++ Chuong_2/algorithms/BinarySearch/binary_search_find_peak_element_inArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int findPeakElement(int arr[], int size) {
+    int left = 0, right = size - 1;
+
+    while(left < right) {
+        int mid = left + (right - left) / 2;
+        if(arr[mid] < arr[mid + 1])
+            left = mid + 1;
+        else
+            right = mid;
+    }
+    return left;
+}
+
+int main() {
+    int arr[] = {1, 3, 20, 4, 1, 0};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int peakIndex = findPeakElement(arr, size);
+
+    std::cout << "The peak element in the array is at index: " << peakIndex << std::endl;
+
+    return 0;
+}
