263bd69cf1c51e4793bc215f49bb8e8a1365317d
Anlh-morgan <9.10hoangan.13072003@gmail.com>
2024-06-23 08:38:45 +0700

    Update binary_search.cpp

--- Chuong_2/algorithms/BinarySearch/binary_search.cpp
+++ Chuong_2/algorithms/BinarySearch/binary_search.cpp
@@ -28,6 +28,8 @@ int binarySearch(int arr[], int size, int target) {
 
 int main() {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50};
+    
+    // Calculate the size of the array
     int size = sizeof(arr) / sizeof(arr[0]);
 
     int target = 10;
