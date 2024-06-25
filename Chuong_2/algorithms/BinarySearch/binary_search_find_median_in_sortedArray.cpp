00a0ccd6e7fd34168f9002cdad8e19de9a4f11f7
Anlh-morgan <9.10hoangan.13072003@gmail.com>
2024-06-25 22:20:32 +0700

    Update binary_search_find_median_in_sortedArray.cpp

--- Chuong_2/algorithms/BinarySearch/binary_search_find_median_in_sortedArray.cpp
+++ Chuong_2/algorithms/BinarySearch/binary_search_find_median_in_sortedArray.cpp
@@ -4,6 +4,7 @@
 #include <climits>
 
 double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
+    // nums1 > nums2 ? swap(nums1, nums2) : nums1;
     if(nums1.size() > nums2.size())
         // Ensure that nums1 is the smaller or equal nums2
         return findMedianSortedArrays(nums2, nums1);
@@ -13,16 +14,17 @@ double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) 
     int left = 0, right = x;
 
     while(left <= right) {
-        int partitionX = left + (right - left) / 2;
-        int partitionY = (x + y + 1) / 2 - partitionX;
+        int partitionX = left + (right - left) / 2; // tính chỉ số phân vùng của nums1
+        int partitionY = (x + y + 1) / 2 - partitionX; // tính chỉ số phân vùng của nums2
 
-        int max_X = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
-        int min_X = (partitionX == x) ? INT_MAX : nums1[partitionX];
+        int max_X = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];  // lấy phần tử lớn nhất của nums1
+        int min_X = (partitionX == x) ? INT_MAX : nums1[partitionX];      // lấy phần tử nhỏ nhất của nums1
 
-        int max_Y = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
-        int min_Y = (partitionY == y) ? INT_MAX : nums2[partitionY];
+        int max_Y = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];  // lấy phần tử lớn nhất của nums2
+        int min_Y = (partitionY == y) ? INT_MAX : nums2[partitionY];      // lấy phần tử nhỏ nhất của nums2
 
         if(max_X <= min_Y && max_Y <= min_X) {
+            // nếu tổng số phần tử là chẵn? return giá trị trung bình của 2 phần tử ở giữa : return giá trị phần tử ở giữa
             if((x + y) % 2 == 0)
                 return(double)(std::max(max_X, max_Y) + std::min(min_X, min_Y)) / 2;
             else
@@ -38,6 +40,7 @@ double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) 
             left = partitionX + 1;
         }
     }
+    // If the input arrays are not sorted
     throw std::invalid_argument("Input arrays are not sorted");
 }
 
