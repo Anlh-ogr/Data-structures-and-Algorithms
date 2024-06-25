e11cd392a7563a584e979bb06229bc94f7fa0a9e
Anlh-morgan <9.10hoangan.13072003@gmail.com>
2024-06-25 22:02:58 +0700

    Update binary_search_find_kth_smallest_element.cpp

--- Chuong_2/algorithms/BinarySearch/binary_search_find_kth_smallest_element.cpp
+++ Chuong_2/algorithms/BinarySearch/binary_search_find_kth_smallest_element.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <vector>
 
+// Function to count the number of elements less than or equal to mid in the matrix
 int countLessEqual(std::vector<std::vector<int>>& matrix, int mid, int n) {
     int count = 0;      // count of elements less than or equal to mid
     
     // start from the bottom-left corner
-    int row = n - 1;
-    int col = 0;
+    int row = n - 1; // last row
+    int col = 0;     // first col   
 
+    // duyệt matrix từ góc dưới bên trái
     while(row>= 0 && col < n) {
+        // phần tử hiện tại <= mid ? tăng biến đếm và chuyển sang cột tiếp theo : di chuyển lên hàng trên
         if(matrix[row][col] <= mid) {
             count += (row + 1);
             col++;
@@ -19,20 +22,24 @@ int countLessEqual(std::vector<std::vector<int>>& matrix, int mid, int n) {
     return count;
 }
 
+// Function to find the kth smallest element in the matrix 
 int kthSmallest(std::vector<std::vector<int>>& matrix, int k) {
     int n = matrix.size();
+    
+    // đặt giá trị biên trái & phải cho max & small elements trong matrix
     int left = matrix[0][0], right = matrix[n-1][n-1];
 
     while(left < right) {
         int mid = left + (right - left) / 2;
-        int count = countLessEqual(matrix, mid, n);
+        int count = countLessEqual(matrix, mid, n); // count of elements less than or equal to mid
 
+        // nếu số phần tử < k? dịch sang trái : dịch sang phải
         if(count < k)
             left = mid + 1;
         else
             right = mid;
     }
-    return left;
+    return left;    // trả về phần tử nhỏ thứ k
 }
 
 int main() {
